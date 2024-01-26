aac36db42b04753ef5ad656ee59d3fe8eab24192
Ashes <105072051+ashes200@users.noreply.github.com>
2024-01-26 10:55:39 -0500

    nav marche

--- Nexus-Vuetify/src/components/NavBar.vue
+++ Nexus-Vuetify/src/components/NavBar.vue
@@ -1,12 +1,37 @@
 <template>
-    <v-app-bar :elevation="21">
-        <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-    
-        <v-app-bar-title>Application Bar</v-app-bar-title>
-      </v-app-bar>
+    <v-app-bar :elevation="21" rounded>
+      <div class="logo">
+				<img :src="logoURL" alt="Vue" />
+			</div>
+      <div class="link">
+        <v-spacer></v-spacer>
+        <v-btn class="betweenLinks" text>Accueil</v-btn>
+        <v-btn class="betweenLinks" text>Boutique</v-btn>
+        <v-btn class="betweenLinks" text>A propos</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text>Connexion</v-btn>
+      </div>
+    </v-app-bar>
 </template>
 
 <script setup>
+  import logoURL from '/src/assets/logo golden transparent.png'
 </script>
+<style scoped>
+  .logo {
+    width: 10%;
+    text-align: center;
+  }
+  .logo > img {
+    width: 75%;
+  }
+  .link {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .link .betweenLinks {
+    flex: 1;
+  }
+</style>
