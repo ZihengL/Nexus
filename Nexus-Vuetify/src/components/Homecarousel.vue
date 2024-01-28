295d72222683541f00760e511f56b693e337b3d1
T3NS31 <amazighmaloum04@gmail.com>
2024-01-28 12:52:06 -0500

    carousel

--- Nexus-Vuetify/src/components/Homecarousel.vue
+++ Nexus-Vuetify/src/components/Homecarousel.vue
@@ -2,37 +2,14 @@
     <v-carousel 
       cycle 
       hide-delimiters 
-      :interval="5000"
+      :interval="5000" 
+      class="carousel"
     >
       <v-carousel-item 
         v-for="(item, i) in items" 
         :key="i"
-      >
-        <v-img :src="item.src">
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-            </v-row>
-          </template>
-  
-          <!-- Navigation bar -->
-          <v-toolbar 
-            color="rgba(0, 0, 0, 0.5)" 
-            dark 
-            absolute 
-            bottom
-          >
-            <v-toolbar-title>{{ item.title }}</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="prev">
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn icon @click="next">
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-toolbar>
-        </v-img>
-      </v-carousel-item>
+        :src="item.src" 
+      ></v-carousel-item>
     </v-carousel>
   </template>
   
@@ -58,4 +35,9 @@
     }
   }
   </script>
+  <style scoped>
+  .carousel{
+    border-top: 100px;
+  }
+  </style>
   
