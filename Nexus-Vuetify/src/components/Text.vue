68745c3d2178b4f92812f782f415272fc8663bcd
T3NS31 <amazighmaloum04@gmail.com>
2024-01-29 10:10:37 -0500

    homepage

--- Nexus-Vuetify/src/components/Text.vue
+++ Nexus-Vuetify/src/components/Text.vue
@@ -1,6 +1,6 @@
 <template>
- <h3>Bienvenue chez Nexus, votre destination ultime pour les jeux vidéo !</h3>
- <p>
+ <h3 class="title">Bienvenue chez Nexus, votre destination ultime pour les jeux vidéo !</h3>
+ <p class="text">
     Chez Nexus, nous partageons votre passion pour le gaming. Notre plateforme est le paradis des joueurs, 
     offrant une vaste sélection de jeux vidéo pour tous les goûts et tous les âges. Que vous soyez un fan de 
     RPG immersifs, de jeux d'action palpitants, ou de simulations réalistes, Nexus vous propose une expérience
@@ -9,4 +9,10 @@
 </template>
 
 <style scoped>
+.title{
+
+}
+.text{
+    
+}
 </style>
