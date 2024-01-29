97b01e3ecbbc26203d4eccf6244a32cc1b07a82b
pruprut <thom.prud@videotron.ca>
2024-01-29 09:59:15 -0800

    ajout de utilisateur + ajout du composant footer

---
+++ Nexus-Vuetify/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted"></span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #f8f9fa;
+  text-align: center;
+  padding: 20px 0;
+}
