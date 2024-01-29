bdb1e7186bbc7639b94bbe3685733daa0d6bcaca
Ashes <105072051+ashes200@users.noreply.github.com>
2024-01-29 11:13:02 -0500

    Update HomeCarousel.vue

--- Nexus-Vuetify/src/components/HomeCarousel.vue
+++ Nexus-Vuetify/src/components/HomeCarousel.vue
@@ -18,6 +18,9 @@ export default {
       { src: './src/assets/BG_IMG/img2.jpg' },
       { src: './src/assets/BG_IMG/logo_shop.png' },
       // Add as many images as you want
+      { src: '../src/assets/img/palworld.png' },
+      { src: '../src/assets/img/tekken.png' },
+      { src: '../src/assets/img/zomboid.png' },
     ],
   }),
 };
