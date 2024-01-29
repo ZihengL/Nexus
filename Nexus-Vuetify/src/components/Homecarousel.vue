51d48e0e1fb5b0705187dcbc0c063e7580990884
T3NS31 <amazighmaloum04@gmail.com>
2024-01-29 10:49:28 -0500

    test

--- Nexus-Vuetify/src/components/Homecarousel.vue
+++ Nexus-Vuetify/src/components/Homecarousel.vue
@@ -15,10 +15,8 @@
 export default {
   data: () => ({
     items: [
-      { src: '../src/assets/img/tekken.png'},
-      { src: '../src/assets/img/palworld.png' },
-      { src: '../src/assets/img/zomboid.png' },
-      { src: '../src/assets/img/elden_ring.png' },
+      { src: './src/assets/BG_IMG/img2.jpg' },
+      { src: './src/assets/BG_IMG/logo_shop.png' },
       // Add as many images as you want
     ],
   }),
