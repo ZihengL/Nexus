4cf82046baf00c81976d1e68132e2e4f43d49e4a
Zi heng Liu <liu.ziheng@yahoo.ca>
2024-01-28 22:30:34 -0500

    Reinstallation of JWT + added gitignore

--- BackEnd/controllers/webtokens.php
+++ BackEnd/controllers/webtokens.php
@@ -3,22 +3,30 @@ require_once 'vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 
+const SECRET_KEY = 'random-key123';
+const ISSUER = 'nexus';
+const AUDIENCE = 'idk';
+
 // Validating credentials and generating token
 function generateToken($user) {
-    $exampleSecretKey = '12345';
-
     $payload = [
-        'iss' => 'your-issuer', // Issuer
-        'aud' => 'your-audience', // Audience
+        'iss' => ISSUER,
+        'aud' => AUDIENCE,
         'iat' => time(), // Issued at
         'exp' => time() + 3600, // Expiration time
-        'sub' => $user['id'] // Subject (user ID)
+        'sub' => $user['id']
     ];
 
-    $jwt = JWT::encode($payload, $exampleSecretKey);
+    $jwt = JWT::encode($payload, SECRET_KEY);
     return $jwt;
 }
 
 function validateToken($jwt) {
-    $decoded = JWT::decode($jwt, 'your_secret_key', array('HS256'));
+    try {
+        $decoded = JWT::decode($jwt, SECRET_KEY, array('HS256'));
+        return $decoded;
+    } catch (Exception $e) {
+        // Handle token validation error, e.g., log or return false
+        return false;
+    }
 }
--- BackEnd/vendor/composer/autoload_psr4.php
+++ BackEnd/vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Stripe\\' => array($vendorDir . '/stripe/stripe-php/lib'),
+    'Firebase\\JWT\\' => array($vendorDir . '/firebase/php-jwt/src'),
 );
