ff64a6fa10c4efdca4b6dd0e5fc72652e0829c68
T3NS31 <amazighmaloum04@gmail.com>
2024-01-29 07:45:23 -0500

    test

--- Nexus-Vuetify/src/components/Homecarousel.vue
+++ Nexus-Vuetify/src/components/Homecarousel.vue
@@ -1,43 +1,24 @@
 <template>
-    <v-carousel 
-      cycle 
-      hide-delimiters 
-      :interval="5000" 
-      class="carousel"
-    >
-      <v-carousel-item 
-        v-for="(item, i) in items" 
-        :key="i"
-        :src="item.src" 
-      ></v-carousel-item>
-    </v-carousel>
-  </template>
-  
-  <script>
-  export default {
-    data () {
-      return {
-        items: [
-          { src: '../src/assets/img/zomboid.png' },
-          { src: '../src/assets/img/elden_ring.png' },
-          { src: '../src/assets/img/palworld.png' },
-          // Add more images as needed
-        ]
-      }
-    },
-    methods: {
-      next() {
-        this.$refs.carousel.next();
-      },
-      prev() {
-        this.$refs.carousel.prev();
-      }
-    }
-  }
-  </script>
-  <style scoped>
-  .carousel{
-    border-top: 100px;
-  }
-  </style>
-  
+  <v-carousel 
+    hide-delimiters 
+    cycle 
+    height="400" 
+    show-arrows-on-hover
+  >
+    <v-carousel-item v-for="(item, i) in items" :key="i">
+      <v-img :src="item.src"></v-img>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { src: './src/assets/BG_IMG/img2.jpg' },
+      { src: './src/assets/BG_IMG/logo_shop.png' },
+      // Add as many images as you want
+    ],
+  }),
+};
+</script>
