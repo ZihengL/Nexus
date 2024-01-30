18620e3d9ae96dbe08ad848cc63031ce0f4bf658
T3NS31 <amazighmaloum04@gmail.com>
2024-01-30 13:57:13 -0500

    avancement

--- Nexus-Vuetify/src/views/Store.vue
+++ Nexus-Vuetify/src/views/Store.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Boutique</h1>
+    <Search/>
+    <Filter/>
+    <Card/>
 </template>
+<script scoped>
+import Card from '../components/Card.vue';
+import Search from '../components/Searchbar.vue';
+import Filter from '../components/Filter.vue';
+export default {
+  components: {
+    Card,
+    Search,
+    Filter,
+  },
+};
+</script>
