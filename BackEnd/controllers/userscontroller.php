84c547075a0ebb0628e991eab93f7f70551c997b
Zi heng Liu <liu.ziheng@yahoo.ca>
2024-01-30 21:48:28 -0500

    User tests

--- BackEnd/controllers/userscontroller.php
+++ BackEnd/controllers/userscontroller.php
@@ -3,68 +3,80 @@
 // require_once "$path/controllers/base_controller.php";
 require_once "$path/models/usermodel.php";
 
-class UsersController {
+class UsersController
+{
     private $model;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->model = new UserModel($pdo);
     }
 
-    public function getUserById($id, $columns = []) {
+    public function getUserById($id, $columns = [])
+    {
         return $this->model->getById($id, $columns);
     }
 
-    public function getUserByEmail($email, $columns = []) {
+    public function getUserByEmail($email, $columns = [])
+    {
         return $this->model->getOne('email', $email, $columns);
     }
 
-    public function getUsersByName($name, $columns = []) {
+    public function getUsersByName($name, $columns = [])
+    {
         return $this->model->get('name', $name, $columns);
     }
 
-    public function getUsersByLastName($lastname, $columns = []) {
+    public function getUsersByLastName($lastname, $columns = [])
+    {
         return $this->model->get('lastname', $lastname, $columns);
     }
 
     // ONLY FOR TESTING, DELETE IN FUTURE
-    public function getAllUsers($columns = []) {
+    public function getAllUsers($columns = [])
+    {
         return $this->model->getAll();
     }
 
     // OTHER CRUDS
 
-    public function createUser($data) {
-       
+    public function createUser($data)
+    {
+
         return $this->model->create($data);
     }
 
-    public function updateUser($id, $data) {
+    public function updateUser($id, $data)
+    {
         $user = $this->model->getById($id);
 
         // if ($this->isAuthenticated() && $user && $user['id'] === $_SESSION['user']['id']) {
-            return $this->model->update($id, $data);
+        return $this->model->update($id, $data);
         // }
 
         // return false;
     }
 
-    public function deleteUser($id) {
+    public function deleteUser($id)
+    {
         $user = $this->model->getById($id);
 
         // if ($this->isAuthenticated() && $user && $user['id'] === $_SESSION['user']) {
-            return $this->model->delete($id);
+        return $this->model->delete($id);
         // }
 
         // return false;
     }
 
-    public function userExists($data) {
+    public function userExists($data)
+    {
         return isset($data['email']) && !empty($this->getUserByEmail($data['email'], ['email']));
     }
 
     // SESSION
 
-    public function login($email, $password) {
+    public function login($email, $password)
+    {
         $user = $this->getUserByEmail($email);
 
         if ($this->verifyUser($user, $email, $password)) {
@@ -79,7 +91,8 @@ class UsersController {
     }
 
 
-    public function logout() {
+    public function logout()
+    {
         if ($this->isAuthenticated()) {
             // session_destroy();
             // session_unset();
@@ -87,13 +100,15 @@ class UsersController {
         }
     }
 
-    private function verifyUser($user, $email, $password) {
+    private function verifyUser($user, $email, $password)
+    {
         return  $user &&
-                $user['email'] === $email &&
-                password_verify($password, $user['password']);
+            $user['email'] === $email &&
+            password_verify($password, $user['password']);
     }
 
-    public function isAuthenticated() {
+    public function isAuthenticated()
+    {
         // TODO: REPLACE SESSIONS WITH WEB TOKENS(JWT).
         // if (session_status() !== PHP_SESSION_ACTIVE) {
         //     session_start();
