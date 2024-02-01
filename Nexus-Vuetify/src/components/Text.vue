10bee6220eee62cc13fc439e8780e45c84cc54ff
Ashes <105072051+ashes200@users.noreply.github.com>
2024-02-01 11:31:32 -0500

    carousel v2

--- Nexus-Vuetify/src/components/Text.vue
+++ Nexus-Vuetify/src/components/Text.vue
@@ -1,16 +1,22 @@
 <template>
- <h2 class="title">Bienvenue chez Nexus, votre destination ultime pour les jeux vidéo !</h2>
- <p class="text">
-    Chez Nexus, nous partageons votre passion pour le gaming. Notre plateforme est le paradis des joueurs, 
-    offrant une vaste sélection de jeux vidéo pour tous les goûts et tous les âges. Que vous soyez un fan de 
-    RPG immersifs, de jeux d'action palpitants, ou de simulations réalistes, Nexus vous propose une expérience
-    d'achat en ligne fluide et agréable.
- </p>
+   <div>
+      <h2 class="title">Bienvenue chez Nexus, votre destination ultime pour les jeux vidéo !</h2>
+      <p class="text">
+         Chez Nexus, nous partageons votre passion pour le gaming. Notre plateforme est le paradis des joueurs,
+         offrant une vaste sélection de jeux vidéo pour tous les goûts et tous les âges. Que vous soyez un fan de
+         RPG immersifs, de jeux d'action palpitants, ou de simulations réalistes, Nexus vous propose une expérience
+         d'achat en ligne fluide et agréable.
+      </p>
+   </div>
 </template>
 
 <style scoped>
+div {
+   border-radius: 16px;
+   margin: 1% 0% 2% 0%;
+   padding: 1% 0% 3% 0%;
+}
 .title{
-   margin-top: 2%;
    margin-bottom: 1%;
    text-align: center;
    color: blueviolet;
