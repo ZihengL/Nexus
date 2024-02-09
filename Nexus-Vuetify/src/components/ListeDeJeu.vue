82ecb22e8baef2850473f38ca7ce365bec3b4538
pruprut <thom.prud@videotron.ca>
2024-02-09 09:30:28 -0800

    Update ListeDeJeu.vue

--- Nexus-Vuetify/src/components/ListeDeJeu.vue
+++ Nexus-Vuetify/src/components/ListeDeJeu.vue
@@ -1,12 +1,37 @@
 <template>
   <!-- <div v-for="activity in activities" :key="activity.id" class="activities">-->
-  <div
-    style="display: flex;justify-content:space-between;border:1px solid black;padding: 20px;border-radius: 30px;box-shadow: 10px 10px 10px black;margin-top: 30px;">
-    <img alt="image jeu" style="align-self: flex-start;">
-    <div style="margin-left: 40px;align-self: flex-end;">
+  <div class="container">
+    <img alt="image jeu" class="img">
+    <div class="jeu">
       <span>Joué à Super Mario Bros le 17/04/2022</span><br />
-      <button style="background-color: blue;padding: 10px;border-radius: 30px;">Voir Plus</button>
+      <button class="btn">Voir Plus</button>
     </div>
   </div>
   <!--</div>-->
 </template>
+<style lang="scss">
+.container {
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  padding: 20px;
+  border-radius: 30px;
+  box-shadow: 10px 10px 10px black;
+  margin-top: 30px;
+}
+
+.img {
+  align-self: flex-start;
+}
+
+.jeu {
+  margin-left: 40px;
+  align-self: flex-end;
+}
+
+.btn {
+  background-color: blue;
+  padding: 10px;
+  border-radius: 30px;
+}
+</style>
