99547c4e55a422c17e37bbd7e01dd603965d0bc7
Ashes <105072051+ashes200@users.noreply.github.com>
2024-02-12 10:10:25 -0500

    merging

--- Nexus-Vuetify/src/components/amis.vue
+++ Nexus-Vuetify/src/components/amis.vue
@@ -1,7 +1,21 @@
 <template>
-  <div style="display: flex; margin-left: 30px; padding: 20px;box-shadow: 10px 10px 10px black;border: 1px solid black;margin-top: 30px;">
-    <img src="" alt="amis" style="margin: 10px;">
+  <div class="amis">
+    <img src="" alt="amis">
     <p>alloooomvjofvbihwabaocvnboj</p>
   </div>
 </template>
+<style lang="scss">
+  .amis {
+    display: flex; 
+    margin-left: 30px; 
+    padding: 20px;
+    box-shadow: 10px 10px 10px black;
+    border: 1px solid black;
+    margin-top: 30px;
+
+    img {
+      margin: 10px;
+    }
+  }
+</style>
 
