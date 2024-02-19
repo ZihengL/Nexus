2be875ea11c1f94ea33cc2fe6dfacdf498ca31f7
pruprut <thom.prud@videotron.ca>
2024-02-19 10:54:59 -0800

    Pour de vrai Avis est fini

--- Nexus-Vuetify/src/components/AvisRating.vue
+++ Nexus-Vuetify/src/components/AvisRating.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="AvisRating">
-    <h2>Avis par la note donnée</h2>
-    <ul>
-      <li v-for="avis in avisParNote" :key="avis.id">
-        {{ avis.text }} - Note: {{ avis.note }}
-      </li>
-    </ul>
+  <div class="glass ratingLast roundBorderSmall">
+    <div class="absolute">
+      <h2>Avis par les meilleurs notes</h2>
+
+      <ul>
+        <li v-for="avis in avisRecents" :key="avis.id" class="glass roundBorderAll padding">
+          <div class="containerStar">
+            <div class="containerAvis">
+              <div class="containerIMG">
+                <img src="../assets/Dev_Picture/amazigh.png" alt="user" class="img" />
+                <p>UserTOTO</p>
+              </div>
+              {{ avis.text }}
+            </div>
+            <div class="ratings">
+              <v-rating hover :length="5" :size="32" :model-value="3" active-color="primary" class="rat" />
+            </div>
+          </div>
+        </li>
+      </ul>
+
+    </div>
   </div>
 </template>
 
@@ -13,18 +28,102 @@
 export default {
   data() {
     return {
-      avisParNote: [ // Supposons que vous avez des données d'avis par note
-        { id: 1, text: "Super produit!", note: 5 },
-        { id: 2, text: "Service client médiocre", note: 2 },
-        { id: 3, text: "Livraison rapide mais produit défectueux", note: 3 }
-      ]
+      avisRecents: [
+        // Supposez que vous avez des données d'avis récents
+        { id: 1, text: "Super produit!" },
+        { id: 2, text: "Excellent service client!" },
+        { id: 3, text: "Livraison rapide et efficace!" },
+      ],
     };
-  }
+  },
 };
 </script>
 
 <style>
-.AvisRating {
+@import "../styles/settings.scss";
+
+.ratingLast {
+  height: 100%;
   flex-basis: 100%;
+  padding: 20px;
+  position: relative;
+
+
+
+  h2 {
+    color: var(--light);
+    margin-bottom: 3%;
+
+  }
+  .padding {
+    padding: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+
+
+    li {
+      width: 100%;
+      padding: 2%;
+
+      color: var(--light);
+      margin-bottom: 20px;
+      border: 1px solid var(--light);
+
+      .containerStar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+
+        .containerAvis {
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
+          align-items: flex-start;
+
+
+          .containerIMG {
+            display: flex;
+            justify-content: center;
+            gap: 10px;
+            align-items: center;
+
+          }
+        }
+      }
+
+      .img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+
+      }
+
+    }
+  }
+
+  .containerFormAvis {
+    margin-top: 15%;
+  }
+
+  .starContainer {
+    margin-top: 2%;
+    display: flex;
+    align-items: center;
+  }
+
+  h3 {
+    color: var(--light);
+  }
+
+  .SaisieText {
+    border: 1px solid white;
+    padding: 2%;
+    width: 100%;
+    height: auto;
+    bottom: 0px;
+  }
 }
 </style>
--- Nexus-Vuetify/src/views/GameVue.vue
+++ Nexus-Vuetify/src/views/GameVue.vue
@@ -1,51 +1,50 @@
 <template>
-    <div id="gameVue">
-        <div class="content">
-            <game class="gameCarrousel"/>
-            <div class="gameInfo glass">
-                <div class="gameImg">
-                    <img src="../assets/image/img1.png" alt="#">
-                </div>
-                <div class="descript">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Vel debitis illum reprehenderit recusandae 
-                        reiciendis quasi magni eum harum minus ipsam sint optio 
-                        quas expedita doloremque maiores enim minima, voluptas unde.</p>
-                </div>
-                <div class="ratings">
-                    <v-rating
-                        hover
-                        :length="5"
-                        :size="32"
-                        :model-value="3"
-                        active-color="primary"
-                        class="rat"
-                    />
-                </div>
-                <div class="devs">
-                    <p><b>Developeur : </b> 2Braise</p>
-                </div>
-                <div class="tags">
-                    <a href="#" class="glow">Fps</a>
-                    <a href="#" class="glow">Aventure</a>
-                    <a href="#" class="glow">Drame</a>
-                    <a href="#" class="glow">Algerient</a>
-                </div>
-                <div class="fieldBtn">
-                    <div class="btn-layer"></div>
-                    <v-btn density="default" class="submit glow" @click="toggleProfile">
-                        Telecharger
-                    </v-btn>
-                </div>
-            </div>
+  <div id="gameVue">
+    <div class="content">
+      <game class="gameCarrousel" />
+      <div class="gameInfo glass">
+        <div class="gameImg">
+          <img src="../assets/image/img1.png" alt="#">
         </div>
-
+        <div class="descript">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Vel debitis illum reprehenderit recusandae
+            reiciendis quasi magni eum harum minus ipsam sint optio
+            quas expedita doloremque maiores enim minima, voluptas unde.</p>
+        </div>
+        <div class="ratings">
+          <v-rating hover :length="5" :size="32" :model-value="3" active-color="primary" class="rat" />
+        </div>
+        <div class="devs">
+          <p><b>Developeur : </b> 2Braise</p>
+        </div>
+        <div class="tags">
+          <a href="#" class="glow">Fps</a>
+          <a href="#" class="glow">Aventure</a>
+          <a href="#" class="glow">Drame</a>
+          <a href="#" class="glow">Algerient</a>
+        </div>
+        <div class="fieldBtn">
+          <div class="btn-layer"></div>
+          <v-btn density="default" class="submit glow" @click="toggleProfile">
+            Telecharger
+          </v-btn>
+        </div>
+      </div>
+    </div>
+    <div class="Avis">
+      <AvisRecent />
+      <AvisRating />
 
     </div>
+
+  </div>
 </template>
 
 <script setup>
-    import game from '../components/GameCarrousel.vue'
+import AvisRating from '@/components/AvisRating.vue';
+import AvisRecent from '@/components/AvisRecent.vue';
+import game from '../components/GameCarrousel.vue';
 </script>
 
 <style src="../styles/GameVueStyle.scss" lang="scss"></style>
