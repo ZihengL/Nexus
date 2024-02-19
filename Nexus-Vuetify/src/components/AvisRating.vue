f4eab9154193bddc356c259ec7e0cabc3185ff84
pruprut <thom.prud@videotron.ca>
2024-02-19 10:49:47 -0800

    Revert "Revert "zone d'avis""
    This reverts commit c29b8215db3563f357784f9cdcdf05cd54ead082.

---
+++ Nexus-Vuetify/src/components/AvisRating.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="AvisRating">
+    <h2>Avis par la note donnée</h2>
+    <ul>
+      <li v-for="avis in avisParNote" :key="avis.id">
+        {{ avis.text }} - Note: {{ avis.note }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      avisParNote: [ // Supposons que vous avez des données d'avis par note
+        { id: 1, text: "Super produit!", note: 5 },
+        { id: 2, text: "Service client médiocre", note: 2 },
+        { id: 3, text: "Livraison rapide mais produit défectueux", note: 3 }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.AvisRating {
+  flex-basis: 100%;
+}
+</style>
---
+++ Nexus-Vuetify/src/components/AvisRecent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ratingLast">
+    <h2>Avis par les derniers sortie</h2>
+    <ul>
+      <li v-for="avis in avisRecents" :key="avis.id">
+        {{ avis.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      avisRecents: [ // Supposez que vous avez des données d'avis récents
+        { id: 1, text: "Super produit!" },
+        { id: 2, text: "Excellent service client!" },
+        { id: 3, text: "Livraison rapide et efficace!" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.ratingLast{
+  flex-basis: 200%;
+}
+ul{
+  list-style-type: none;
+}
+</style>
