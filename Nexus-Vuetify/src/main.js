196f4e3c7c8bdcb2616e7ea3be1f2d24782c6871
T3NS31 <amazighmaloum04@gmail.com>
2024-02-26 11:06:43 -0500

    firebase_implementation

--- Nexus-Vuetify/src/main.js
+++ Nexus-Vuetify/src/main.js
@@ -1,35 +1,52 @@
-/**
- * main.js
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+// main.js
 
-// Plugins
-import { registerPlugins } from '@/plugins'
+// Import Vue and Vuetify plugins
 import { createApp, ref, provide } from 'vue';
+import { registerPlugins } from '@/plugins';
 
-// Components
+// Import App component
 import App from './App.vue';
 
-// Components
+// Import Firebase functionalities
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2l5gdjn5nRUPEYWZwUnbJM1Fyr0aL2IY",
+  authDomain: "nexus-414517.firebaseapp.com",
+  projectId: "nexus-414517",
+  storageBucket: "nexus-414517.appspot.com",
+  messagingSenderId: "155427684847",
+  appId: "G-4EGMVSZE3G",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// Now that Firebase is initialized, you can access Firebase Storage
+// or other Firebase services where needed in your app
+const storage = getStorage();
+
+// Create Vue app
 const app = createApp(App)
 
-// Utilisez app.config.globalProperties pour définir une propriété globale
-//app.config.globalProperties.$isConnected = ref(false);
-
-// Création de l'état global isConnected
+// Utilize app.config.globalProperties to define a global property
+// This example shows how to handle a global state like isConnected
 const isConnected = ref(false);
-// Méthode pour basculer l'état
 const toggleConnection = () => {
   isConnected.value = !isConnected.value;
 };
 
-// Fournir l'état et la méthode aux composants descendants
+// Provide the state and method to descendant components
 app.provide('isConnected', isConnected);
 app.provide('toggleConnection', toggleConnection);
 
-// Enregistrez les plugins
+// Register Vuetify and other plugins
 registerPlugins(app)
 
-// Montez l'application
+// Mount the Vue application
 app.mount('#app')
+
+// Optionally, export storage if you need to use it across components
+export { storage };
