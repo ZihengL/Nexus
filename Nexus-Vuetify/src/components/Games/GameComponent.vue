fc0e2f743760640dd60ef5d5b8632095c2c55cd8
Zi heng Liu <liu.ziheng@yahoo.ca>
2024-02-27 19:56:50 -0500

    half-assed vuetify stuff

---
+++ Nexus-Vuetify/src/components/Games/GameComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card :id="game.id" class='game'>
+        <h1>{{ game.title }}</h1>
+        <p>{{ game.description }}</p>
+    </v-card>
+</template>
+
+
+<script setup>
+import { defineProps } from "vue";
+
+const game = defineProps({
+    id: Number,
+    developerID: Number,
+    stripeID: Number,
+    title: String,
+    description: String,
+    files: String,
+    media: String,
+    releaseDate: Date,
+    ratingAverage: Number
+});
+
+const getDeveloper = async () => {
+    const data = null;
+};
+
+</script>
