8955b67ef8609603481eac02cf9335410cc990ef
Ashes <105072051+ashes200@users.noreply.github.com>
2024-02-28 15:24:35 -0500

    maj liste game

--- Nexus-Vuetify/src/components/login/ListeDeJeu.vue
+++ Nexus-Vuetify/src/components/login/ListeDeJeu.vue
@@ -5,8 +5,8 @@
       <img src="../../assets//img/apex.png" alt="image jeu" class=" roundBorderSmall">
     </div>
     <div class="jeu">
-      <span v-if="props.himself && !props.buy">Joué à {{ LeGame}} le 17/04/2022</span>
-      <span v-if="props.himself && props.buy">{{ LeGame}} : televerser le 17/04/2022</span>
+      <span v-if="props.himself && !props.buy">{{ LeGame}} : televerser le 17/04/2022 </span>
+      <span v-if="props.himself && props.buy">Joué à {{ LeGame}} le 17/04/2022</span>
       <span v-else>{{ LeGame }}</span>
       <br />
       <div class="fieldBtn">
