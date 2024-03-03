432a318c6464b80d374f018ed241a1088ec2883d
Zi heng Liu <liu.ziheng@yahoo.ca>
2024-03-03 17:17:36 -0500

    partial amend of previous commit

--- Nexus-Vuetify/src/components/Devs/Thomas.vue
+++ Nexus-Vuetify/src/components/Devs/Thomas.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="profile-container">
-    <div class="profile-info">
-      <img class="profile-picture" src="../../assets/Dev_Picture/Thomas.png" alt="Photo de Thomas Prud'Homme" />
-      <div class="profile-text">
+    <div class="profile-info glass roundBorderAll">
+      <img class="profile-picture roundBorderAll" src="../../assets/Dev_Picture/Thomas.png"
+        alt="Photo de Thomas Prud'Homme" />
+      <div class="profile-text glass3 roundBorderAll">
         <h1 class="profile-name">Thomas Prud'homme</h1>
-        <h2 class="profile-title">Développement front-end et back-end</h2>
+        <h2 class="profile-title">Développeur front-end et back-end</h2>
         <p class="profile-description">
           En tant que développeur web, Thomas excelle dans l'art de transformer des idées complexes en solutions
           fonctionnelles
           et intuitives. Sa passion pour le codage se manifeste à travers des sites web dynamiques et réactifs qui
           captivent les
-          utilisateurs dès leur première visite. Grâce à son expertise technique et sa créativité, il crée des expériences
+          utilisateurs dès leur première visite. Grâce à son expertise technique et sa créativité, il crée des
+          expériences
           en
-          ligne fluides et engageantes, propulsant ainsi les projets de ses clients vers de nouveaux sommets dans le monde
+          ligne fluides et engageantes, propulsant ainsi les projets de ses clients vers de nouveaux sommets dans le
+          monde
           numérique.
         </p>
         <blockquote class="profile-quote">“L’innovation est le moteur de la créativité et de la transformation.”
         </blockquote>
         <cite class="profile-cite">Marc Benioff</cite>
         <div class="profile-links">
-          <a class="profile-link" href="https://github.com/votreutilisateur">Github</a>
-          <a class="profile-link" href="mailto:votreemail@example.com">Email</a>
-          <a class="profile-link" href="https://www.linkedin.com/in/thomas-prud-homme-7921b8261/">Linkedin</a>
+          <div class="fieldBtn">
+            <div class="btn-layer"></div>
+            <v-btn density="default" class="submit" href="https://github.com/pruprut?tab=repositories">
+              Github
+            </v-btn>
+          </div>
+          <div class="fieldBtn">
+            <div class="btn-layer"></div>
+            <v-btn density="default" class="submit" href="mailto:thom.prud@videotron.ca">
+              Email
+            </v-btn>
+          </div>
+          <div class="fieldBtn">
+            <div class="btn-layer"></div>
+            <v-btn density="default" class="submit" href="https://www.linkedin.com/in/thomas-prud-homme-7921b8261/">
+              Linkedin
+            </v-btn>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <style scoped>
+.profile-info {
+  display: flex;
+  padding: 5%
+}
+
 .profile-container {
   max-width: 800px;
   margin: 0 auto;
@@ -81,6 +105,8 @@
 
 .profile-links {
   margin-top: 20px;
+  display: flex;
+  gap: 5%;
 }
 
 .profile-link {
@@ -104,8 +130,8 @@
 
 /* Effet de survol sur l'image */
 .profile-picture {
-  width: 200px;
-  height: auto;
+  width: 30%;
+  height: 30%;
   margin-right: 20px;
   transition: transform 0.3s ease;
   /* Transition de transformation sur 0.3s avec un effet d'accélération */
@@ -118,13 +144,13 @@
 
 .profile-text {
   transition: color 0.3s ease;
+  padding: 5%;
+  color: var(--light-trans-2);
+
   /* Transition de couleur sur 0.3s avec un effet d'accélération */
 }
 
-.profile-text:hover {
-  color: #ff7f50;
-  /* Nouvelle couleur au survol */
-}
+
 
 .profile-background {
   background-image: url('votre-image.jpg');
@@ -155,5 +181,6 @@
   /* Nouvelle couleur au survol */
 }
 </style>
+
 <script>
 </script>
