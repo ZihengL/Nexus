1a12a79396923a4b4d7bf0999eca563386a07893
Rebecca Dauphnie Mombrun <joceline1233@gmail.com>
2024-03-11 05:12:55 -0400

    img thing

--- Nexus-Vuetify/src/components/store/SingleGameComponent.vue
+++ Nexus-Vuetify/src/components/store/SingleGameComponent.vue
@@ -115,6 +115,8 @@ onMounted(async () => {
   .img {
     flex: 6;
     position: relative;
+    width: 100%;
+    height: 100%;
 
     img {
       width: 100%;
@@ -122,6 +124,7 @@ onMounted(async () => {
       display: inline-block;
       transition: opacity 0.3s ease;
       opacity: 1;
+      object-fit: fill; 
     }
 
     p {
